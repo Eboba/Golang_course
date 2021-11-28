dea5e8b5d255a05c27dec355d1f1268bc64b7349
Eboba <opex99@mail.ru>
2021-11-28 12:55:55 +0300

    ikjhk

---
+++ task21.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var CircleNumber int = 4
+	var Speed int = 358
+	var Engine int = 254
+	var Wheels int = 93
+	var Eudder int = 49
+	var Wing int = 21
+	var Rain int = 14
+
+	fmt.Println("==============")
+	fmt.Println("Супер Гонки", "Круг", CircleNumber)
+	fmt.Println("==============")
+	fmt.Print("Шумахер (", Speed, ")", "\n")
+	fmt.Println("==============")
+	fmt.Println("Водитель: Шумахер", "скорость:", Speed)
+	fmt.Println("-------------")
+	fmt.Println("Оснащение")
+	fmt.Println("Двигатель:", Engine)
+	fmt.Println("Колеса:", Wheels)
+	fmt.Println("Руль:", Eudder)
+	fmt.Println("-------------")
+	fmt.Println("Действия плохой погоды")
+	fmt.Println("Ветер:", Wing)
+	fmt.Println("Дождь:", Rain)
+}
